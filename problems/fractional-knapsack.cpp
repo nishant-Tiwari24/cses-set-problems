e583b248459e0cf6ca87f12c1ed6a7b08f18d8ff
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-02-20 12:25:37 +0530

    fractional-knapscak

---
+++ problems/fractional-knapsack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class fraction {
+public:
+    int profit;
+    int weight;
+};
+
+int main() {
+    int n;
+    cin>>n;
+    fraction obj[n];
+    
+    for(int i=0;i<n;i++) {
+        cin>>obj.profit[i]>>obj->weight[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        
+    }
+}
